f28216c5cdf3ec3bcfd86bb77c5cb2597d2e6e82
yoon <yunyun40550082@gmail.com>
2022-05-18 03:09:28 +0900

    feat: 사용성 개선 - input자동 포커스 & 입력 텍스트 강조 처리

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import Component from '../core/Component.js';
 class Input extends Component {
   template() {
     return `
-         <input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세요." value="">
+         <input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세요." value="" autofocus>
         `;
   }
 
